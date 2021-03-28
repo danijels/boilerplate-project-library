41abec335a5a249134a88d5f39399f29bc299e25
danijels <73341306+danijels@users.noreply.github.com>
2021-03-28 16:36:16 +0200

    Fix get book error message

--- public/client.js
+++ public/client.js
@@ -35,6 +35,14 @@ async function showBook(passedId) {
 
   const data = await fetch(`/api/books/${passedId}`, { method: 'GET' });
   const book = await data.json();
+
+  if (book.error) {
+    bookDetail.style.visibility = 'hidden';
+    messages.style.visibility = 'visible';
+    messages.innerText = 'Sorry, we couldn\'t find this book...'
+    return;
+  }
+  
   messages.style.visibility = 'hidden';
   bookDetail.style.visibility = 'visible';
   bookTitle.innerText = book.title;
--- routes/api.js
+++ routes/api.js
@@ -91,5 +91,4 @@ module.exports = function (app) {
         res.json('delete successful');
       })
     });
-  
 };
